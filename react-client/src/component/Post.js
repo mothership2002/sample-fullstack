97a7e39dc3fbe379e2a3798e837c4a096b1c8e46
mothership2002 <mothership2002@naver.com>
2023-05-21 00:34:01 +0900

    포스트 퍼블리싱

--- react-client/src/component/Post.js
+++ react-client/src/component/Post.js
@@ -3,13 +3,26 @@ import React from 'react'
 const Post = () => {
   const boardCss = 'border border-danger border-2 opacity-50'
   return (
-    <div className='border border-danger border-2 opacity-50 '
-    style={{height:'20893px'}}>
-      <div>
-        title
-      </div>
-      <div>
-        creater
+    <div className={boardCss + ' d-flex flex-column'}
+      style={{ height: '20893px' }}>
+      <div className={boardCss + ' d-flex justify-content-md-between'}>
+        <div style={{minWidth:'75%'}}>
+          title
+        </div>
+
+        <div className={boardCss + ' d-flex flex-md-column '}
+        style={{minWidth: '25%'}}>
+          <div>
+            creater
+          </div>
+          <div>
+            created date
+          </div>
+          <div>
+            modified date
+          </div>
+        </div>
+
       </div>
       <div>
         content
