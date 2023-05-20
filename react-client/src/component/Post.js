910b4ea8da35e6f33d2bfa676a95a4a8c9b6f8a2
mothership2002 <mothership2002@naver.com>
2023-05-21 00:47:48 +0900

    포스트 퍼블리싱

--- react-client/src/component/Post.js
+++ react-client/src/component/Post.js
@@ -1,25 +1,34 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
 
 const Post = () => {
   const boardCss = 'border border-danger border-2 opacity-50'
   return (
     <div className={boardCss + ' d-flex flex-column'}
       style={{ height: '20893px' }}>
-      <div className={boardCss + ' d-flex justify-content-md-between'}>
-        <div style={{minWidth:'75%'}}>
+      <div className={boardCss + ' d-flex justify-content-md-between'} style={{maxHeight:'6.58rem'}}>
+        <div style={{minWidth:'75%'}} className='d-inline-flex p-3 align-items-md-center flex-md-wrap'>
           title
         </div>
 
-        <div className={boardCss + ' d-flex flex-md-column '}
+        <div className={boardCss + ' d-inline-flex flex-md-column flex-md-wrap'}
         style={{minWidth: '25%'}}>
-          <div>
+          <div className='d-inline-flex p-1 align-items-md-center'>
             creater
           </div>
-          <div>
+          <div className='d-inline-flex p-1 align-items-md-center'>
             created date
           </div>
-          <div>
+          <div className='d-inline-flex p-1 align-items-md-center'>
             modified date
+          </div>
+          <div className='d-inline-flex flex-column justify-content-md-between' style={{minHeight:'100%'}}>
+            <Button>
+              hello
+            </Button>
+            <Button>
+              world
+            </Button>
           </div>
         </div>
 
