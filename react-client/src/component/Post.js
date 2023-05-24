f6c41039292807cd96b969e7cdf614e07826433e
mothership2002 <mothership2002@naver.com>
2023-05-24 23:07:08 +0900

    스크롤 연구, css 모듈 분리

--- react-client/src/component/Post.js
+++ react-client/src/component/Post.js
@@ -1,54 +1,84 @@
 import React, { useEffect, useState } from 'react'
 import { Button, Placeholder } from 'react-bootstrap';
-
-const ContentComponent = (element) => {
-  console.log(element.obj.postId.toString());
-  return (
-    <div className={'d-flex flex-column m-2'} style={{ minHeight: '500px' }}>
-      <div className={'border border-danger border-2 d-flex justify-content-md-between rounded'}
-        style={{ maxHeight: '6.58rem' }} key={element.obj.postId}>
-        <div className='d-inline-flex p-3 align-items-md-center flex-md-wrap title' key={element.obj.postId}>
-          {element.obj.postTitle}
-        </div>
-        <div className={'d-inline-flex flex-md-column flex-md-wrap'} style={{ minWidth: '25%' }}>
-          <div style={{minWidth:'40%', maxWidth:'50%', height:'100%'}} className='text-wrap d-flex flex-column justify-content-center'>
-            <div className='d-inline-flex p-1 align-items-md-center text-wrap fw-lighter created-info'>
-              {element.obj.creater}
-            </div>
-            <div className='d-inline-flex p-1 align-items-md-center text-wrap created-info fw-lighter'>
-              {element.obj.createdAt}
-            </div>
-            <div className='d-inline-flex p-1 align-items-md-center created-info fw-lighter'>
-              {element.obj.modifiedAt}
-            </div>
-          </div>
-          <div className='d-inline-flex flex-column justify-content-md-evenly ' style={{ minHeight: '100%', minWidth:'50%' }}>
-            {element.obj.buttonArea ?
-              element.obj.buttonArea :
-              <>
-                <Button bsPrefix='btn btn-outline-primary btn-sm m-2'>
-                  Modify
-                </Button>
-                <Button bsPrefix='btn btn-outline-danger btn-sm m-2'>
-                  Delete
-                </Button>
-              </>
-            }
-          </div>
-        </div>
-      </div>
-      <div style={{minHeight:'350px'}} className='p-3'>
-        {element.obj.postContent}
-      </div>
-      <div className={'p-3 border border-danger border-2'} style={{minHeight:'150px'}}>
-        coment 이건 생각해봐야겟다
-      </div>
-      
-    </div>
-  )
-}
+import { useRecoilValue } from 'recoil';
+import { postCss } from '../stores/css/PostCss';
+import Utils from '../common/Utils';
 
 const Post = () => {
+
+  // css
+  const css = useRecoilValue(postCss);
+  const utils = new Utils();
+
+  const [mockList, setMockList] = useState([]);
+  const [postList, setPostList] = useState([]);
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    setMockList(preList => [...preList, PlaceholderObject(-1), PlaceholderObject(-2)]);
+    selectPostList();
+
+    window.addEventListener('scroll', onscroll);
+    return () => {
+      window.removeEventListener('scroll', onscroll);
+    }
+  }, [])
+  
+  const onscroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    console.log(scrollPosition);
+  }
+
+  const ContentComponent = (element) => {
+    return (
+      <div className={utils.returnCssClassArray(css.postContainer)}>
+        <div className={utils.returnCssClassArray(css.postInfoContainer)}>
+          <div className={utils.returnCssClassArray(css.postTitle)}>
+            {element.obj.postTitle}
+          </div>
+          <div className={utils.returnCssClassArray(css.postDetailInfoContainer)}>
+            <div className={utils.returnCssClassArray(css.dateContainer)}>
+              <div className={utils.returnCssClassArray(css.createInfo)}>
+                {element.obj.creater}
+              </div>
+              <div className={utils.returnCssClassArray(css.createInfo)}>
+                {element.obj.createdAt}
+              </div>
+              <div className={utils.returnCssClassArray(css.createInfo)}>
+                {element.obj.modifiedAt}
+              </div>
+            </div>
+            <div className={utils.returnCssClassArray(css.btnArea)}>
+              {element.obj.buttonArea ?
+                element.obj.buttonArea :
+                <>
+                  <Button bsPrefix={btnCss('primary')}>
+                    Modify
+                  </Button>
+                  <Button bsPrefix={btnCss('danger')}>
+                    Delete
+                  </Button>
+                </>
+              }
+            </div>
+          </div>
+        </div>
+        <div className={utils.returnCssClassArray(css.postContentContainer)}>
+          {element.obj.postContent}
+        </div>
+        <div className={'p-3 border border-secondary border-2 m-1 rounded' } style={{minHeight:'150px'}}>
+          coment 이건 생각해봐야겟다
+        </div>
+        
+      </div>
+    )
+  }
+
+  const btnCss = (string) => {
+    return 'btn btn-outline-' + string +' btn-sm m-2';
+  }
+  
   ///////////////////////////sample data///////////////////////////////////////
   const sampleSync = (ms) => {
     return new Promise((resolve) => {
@@ -97,14 +127,6 @@ const Post = () => {
     setPostList(preList => [...preList, ...data]);
   }
   ///////////////////////////sample data///////////////////////////////////////
-  const [mockList, setMockList] = useState([]);
-  const [postList, setPostList] = useState([]);
-  
-  useEffect(() => {
-    setMockList(preList => [...preList, PlaceholderObject(-1), PlaceholderObject(-2)]);
-    selectPostList();
-  }, [])
-
 
   /** 플레이스 홀더 생성 
    * 
@@ -131,7 +153,7 @@ const Post = () => {
   const PlaceholderObject = (id) => {
     return {
       postId: 'pk'+ id,
-      postTitle: randomPlaceholder(2, 'd-inline-flex align-items-md-center flex-md-wrap title'),
+      postTitle: randomPlaceholder(2, utils.returnCssClassArray(css.placeholderContainer)),
       creater: <Placeholder xs={7} bg='secondary' size='lg' />,
       createdAt: <Placeholder xs={10} bg='secondary' size='lg'/>,
       modifiedAt: <Placeholder xs={9} bg='secondary' size='lg' />,
@@ -145,20 +167,18 @@ const Post = () => {
     }
   }
 
-  const renderingComponet = postList.map((element) => {
-    return (
-      <ContentComponent obj={element} key={element.postId.toString()} />
-    )
-  })
-  
+  const renderingComponet = (array) => {
+    return array.map((element) => {
+      return (
+        <ContentComponent obj={element} key={element.postId.toString()} />
+      )
+    })
+  }
+
   return (
     <>
-      {renderingComponet}
-      {mockList.map((element) => {
-        return (
-          <ContentComponent obj={element} key={element.postId}/>
-        )
-      })}
+      {renderingComponet(postList)}
+      {renderingComponet(mockList)}
     </>
   )
 }
--- react-client/src/stores/css/PostCss.js
+++ react-client/src/stores/css/PostCss.js
@@ -3,6 +3,79 @@ import { atom } from "recoil";
 export const postCss = atom({
   key: 'postCss',
   default: {
+    postContainer: [
+      'd-flex',
+      'flex-column',
+      'm-2',
+      'min-height',
+    ],
 
+    postInfoContainer: [
+      'border',
+      'border-secondary',
+      'border-2',
+      'd-flex',
+      'justify-content-md-between',
+      'rounded',
+      'm-1',
+      'post-info-container',
+    ],
+
+    postTitle: [
+      'd-inline-flex',
+      'p-3',
+      'align-items-md-center',
+      'flex-md-wrap',
+      'title'
+    ],
+
+    postDetailInfoContainer: [
+      'd-inline-flex',
+      'flex-md-column',
+      'flex-md-wrap',
+      'post-detail-info-container'
+    ],
+
+    dateContainer: [
+      'text-wrap',
+      'd-flex',
+      'flex-column',
+      'justify-content-center',
+      'date-container'
+    ],
+
+    createInfo: [
+      'd-inline-flex',
+      'p-1',
+      'align-items-md-center',
+      'text-wrap',
+      'created-info',
+      'fw-lighter'
+    ],
+
+    btnArea: [
+      'd-inline-flex',
+      'flex-column',
+      'justify-content-md-evenly',
+      'btn-area'
+    ],
+
+    postContentContainer: [
+      'p-3',
+      'm-1',
+      'border',
+      'border-secondary',
+      'border-2',
+      'rounded',
+      'bg-light',
+      'post-content-container'
+    ],
+    
+    placeholderContainer: [
+      'd-inline-flex',
+      'align-items-md-center',
+      'flex-md-wrap',
+      'title',
+    ]
   }
 });
